46984dbc6df44131b3ec986e17de0b6402f7b3ab
Aline Tavares <alaisgomes@gmail.com>
2023-02-17 10:38:54 -0500

    Changes required to have the app run

--- screens/Untitled3/index.js
+++ screens/Untitled3/index.js
@@ -1,37 +1,59 @@
-import { boredapi20_get_activity_read } from "./../../store/boredAPI/boredapi20_response_get_GetActivities.slice.js";
-import { useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { useSelector } from "react-redux";
-import { Text } from "react-native";
-import React from "react";
-import { View, StyleSheet, ScrollView, SafeAreaView } from "react-native";
+import { boredapi20_get_activity_read } from "./../../store/boredAPI/boredapi20_response_get_GetActivities.slice.js"
+import { useEffect } from "react"
+import { useDispatch } from "react-redux"
+import { useSelector } from "react-redux"
+import { Text } from "react-native"
+import React from "react"
+import { View, StyleSheet, ScrollView, SafeAreaView } from "react-native"
 
 const Untitled3 = () => {
-  const dispatch = useDispatch();
+  const dispatch = useDispatch()
   useEffect(() => {
-    dispatch(boredapi20_get_activity_read());
-  }, []);
-  const boredapi20_response_get_GetActivity = useSelector(state => state.boredapi20_response_get_GetActivity);
-  return <SafeAreaView style={styles.safeArea}>
-        <ScrollView contentContainerStyle={{
-      flex: 1,
-      flexDirection: 'column',
-      height: '100%',
-      padding: 10,
-      backgroundColor: '#f0f0f1'
-    }}>
-        
-          <View style={styles.column1}><Text style={styles.qJyexdZm}>An Activity Suggestion For You</Text></View>
-          <View style={styles.column2}><Text style={styles.FZYairKB}>{boredapi20_response_get_GetActivity.activity}</Text></View>
-          
-        
-        </ScrollView>
-      </SafeAreaView>;
-};
+    dispatch(
+      // added this: my action required parameters!
+      boredapi20_get_activity_read({
+        participants: 5,
+        price: 400,
+        minprice: 0,
+        maxprice: 400
+      })
+    )
+  }, [])
+  // changed this: we astore in an entities array. Also, name of model was wrong in dashboard
+  const { entities: Boredapi20_response_get_GetActivities } = useSelector(
+    state => state.Boredapi20_response_get_GetActivities
+  )
+
+  return (
+    <SafeAreaView style={styles.safeArea}>
+      <ScrollView
+        contentContainerStyle={{
+          flex: 1,
+          flexDirection: "column",
+          height: "100%",
+          padding: 10,
+          backgroundColor: "#f0f0f1"
+        }}
+      >
+        <View style={styles.column1}>
+          <Text style={styles.qJyexdZm}>An Activity Suggestion For You</Text>
+        </View>
+        <View style={styles.column2}>
+          <Text style={styles.FZYairKB}>
+            {/* Added a check for possible empty array */}
+            {Boredapi20_response_get_GetActivities.length
+              ? Boredapi20_response_get_GetActivities[0].activity
+              : ""}
+          </Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  )
+}
 
 const styles = StyleSheet.create({
   safeArea: {
-    height: '100%'
+    height: "100%"
   },
   column1: {
     flex: 1
@@ -58,5 +80,5 @@ const styles = StyleSheet.create({
     fontSize: 20,
     borderRadius: 0
   }
-});
-export default Untitled3;
+})
+export default Untitled3
